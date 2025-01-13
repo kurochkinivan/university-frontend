ed2c323bdb00386710d70859f107edebfd11da9f
kurochkinivan <vanya.kurochkin.23@mail.ru>
2025-01-13 19:39:43 +0700

    change files

--- vue/vue/src/components/About/AboutSection.vue
+++ vue/vue/src/components/About/AboutSection.vue
@@ -5,11 +5,6 @@
             <div class="row align-items-center">
                 <div class="col-lg-6 col-12">
                     <div class="ratio ratio-1x1">
-                        <video autoplay="" loop="" muted="" class="custom-video" poster="">
-                            <source src="/videos/pexels-mike-jones-9046237.mp4" type="video/mp4" />
-
-                            Your browser does not support the video tag.
-                        </video>
 
                         <div class="about-video-info d-flex flex-column">
                             <h4 class="mt-auto">We Started Since 2009.</h4>
